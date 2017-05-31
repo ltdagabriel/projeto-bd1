2a5e870150b26aa80fa8c4dc9b2af30ea9bf0a57
Venancius Lenz Michelan <venanciuslm@gmail.com>
2017-05-31 19:06:21 -0300

    Adicionado senha

--- Estrutura/sql_projeto.sql
+++ Estrutura/sql_projeto.sql
@@ -16,6 +16,7 @@ create table Pessoa(
     nome varchar (40),
     tel integer,
     rua_endereco varchar (50),
+    senha varchar(20),
     primary key (cpf),
     foreign key (rua_endereco) references Endereco(rua)
 );
@@ -38,14 +39,6 @@ create table Veiculo(
     veiculo_tipo integer NOT NULL AUTO_INCREMENT,
     primary key (placa),
     foreign key (veiculo_tipo) references Tipo(id)
-);
-	
-create table Orcamento(
-	id integer NOT NULL AUTO_INCREMENT,
-    data_Orc date,
-    cpf_cliente varchar (11),
-    primary key (id),
-    foreign key (cpf_cliente) references Cliente(cpf)
 );
 
 create table Tarefa(
@@ -71,6 +64,16 @@ create table Funcionario(
     foreign key (id_funcao) references Funcao(id)
 );
 
+create table Orcamento(
+	id integer NOT NULL AUTO_INCREMENT,
+    data_Orc date,
+    cpf_cliente varchar (11),
+    cpf_funcionario varchar(11),
+    primary key (id),
+    foreign key (cpf_cliente) references Cliente(cpf),
+    foreign key (cpf_funcionario) references Funcionario(cpf)
+);
+
 create table Cliente_Veiculo(
 	cpf_cliente varchar (11),
     placa_veiculo varchar (7),
@@ -84,13 +87,6 @@ create table Estoque(
     qtde integer,
     preco float,
     primary key (id)
-);
-
-create table Orcamento_Funcionario(
-	id_orcamento integer,
-    cpf_funcionario varchar (11),
-    foreign key (id_orcamento) references Orcamento(id),
-    foreign key (cpf_funcionario) references Funcionario(cpf)
 );
 
 create table Orcamento_Tarefa(
@@ -115,25 +111,37 @@ insert into Endereco
 values ("Avenida dos Andradas",234,87310680,"Campo Mourão","PR");
 insert into Endereco
 values ("Conjunto Benevides",918,87308155,"Campo Mourão","PR");
+insert into Endereco
+values ("Rua Avelina Sanson Cooper",115,82220144,"Curitiba","PR");
+insert into Endereco
+values("Travessa Luiz Tomazi",733,85605351,"Campo Mourão","PR");
 
 insert into Pessoa
-values ("22570989517","Ryan Paulo Vinicius Lima",38223790,"Rua A");
+values ("22570989517","Ryan Paulo Vinicius Lima",38223790,"Rua A","123456");
 insert into Pessoa
-values ("56027372192","Iago Renato Monteiro",36477694,"Rua Jango Menezes");
+values ("56027372192","Iago Renato Monteiro",36477694,"Rua Jango Menezes","123456");
 insert into Pessoa
-values ("00415965870","Matheus Giovanni Nathan Carvalho",38791387,"Avenida dos Andradas");
+values ("00415965870","Matheus Giovanni Nathan Carvalho",38791387,"Avenida dos Andradas","123456");
 insert into Pessoa
-values ("87449101424","Débora Julia Bárbara da Silva",27196380,"Conjunto Benevides");
+values ("87449101424","Débora Julia Bárbara da Silva",27196380,"Conjunto Benevides","123456");
+insert into Pessoa
+values ("18740148920","Elisa Nicole Nascimento",988018452,"Rua Avelina Sanson Cooper","123456");
+insert into Pessoa
+values ("86014403981","Isaac Elias Rodrigues",25284227,"Travessa Luiz Tomazi","123456");
 
 insert into Cliente
 values ("22570989517");
 insert into Cliente
 values ("56027372192");
+insert into Cliente
+values ("18740148920");
 
 insert into Tipo
 values (DEFAULT,"Carro","Gol");
 insert into Tipo
 values (DEFAULT,"Moto","Ybr");
+insert into Tipo
+values(DEFAULT,"Carro","Camaro");
 
 insert into Funcao
 values (DEFAULT,"Mecanico");
@@ -144,26 +152,43 @@ insert into Veiculo
 values ("APY9974",DEFAULT);
 insert into Veiculo
 values ("KEO7537",DEFAULT);
-
-insert into Orcamento
-values (DEFAULT,STR_TO_DATE('01-01-2017', '%d-%m-%Y'),22570989517);
-insert into Orcamento
-values (DEFAULT,STR_TO_DATE('02-01-2017', '%d-%m-%Y'),56027372192);
+insert into Veiculo
+values ("ARP6834",DEFAULT);
 
 insert into Tarefa
 values (DEFAULT,"Trocar pneu",300);
 insert into Tarefa
 values (DEFAULT,"Trocar bateria",200);
+insert into Tarefa
+values (DEFAULT,"Trocar radiador",450);
 
 insert into Estoque
 values (DEFAULT,"Bateria 1",20,150);
 insert into Estoque
 values (DEFAULT,"Pneu 1",16,200);
+insert into Estoque
+values (DEFAULT,"Radiador 1",13,250);
 
 insert into Tarefa_Estoque
 values (1,2);
 insert into Tarefa_Estoque
 values (2,1);
+insert into Tarefa_Estoque
+values (3,3);
+
+insert into Funcionario
+values ("00415965870",111111,2200,1);
+insert into Funcionario
+values ("87449101424",222222,1800,2);
+insert into Funcionario
+values ("86014403981",333333,2000,1);
+
+insert into Orcamento
+values (DEFAULT,STR_TO_DATE('01-01-2017', '%d-%m-%Y'),"22570989517","00415965870");
+insert into Orcamento
+values (DEFAULT,STR_TO_DATE('02-01-2017', '%d-%m-%Y'),"56027372192","00415965870");
+insert into Orcamento
+values (DEFAULT,STR_TO_DATE('02-01-2017', '%d-%m-%Y'),"18740148920","00415965870");
 
 insert into Orcamento_Tarefa
 values (1,1);
@@ -171,14 +196,12 @@ insert into Orcamento_Tarefa
 values (1,2);
 insert into Orcamento_Tarefa
 values (2,1);
-
-
-insert into Funcionario
-values ("00415965870",111111,2200,1);
-insert into Funcionario
-values ("87449101424",222222,1800,2);
+insert into Orcamento_Tarefa
+values (3,3);
 
 insert into Cliente_Veiculo
 values ("22570989517","APY9974");
 insert into Cliente_Veiculo
 values ("56027372192","KEO7537");
+insert into Cliente_Veiculo
+values ("18740148920","ARP6834");
