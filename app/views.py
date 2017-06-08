3befe17ca1acb08a5165ef3011ad72461f4295f8
Venancius Lenz Michelan <venanciuslm@gmail.com>
2017-06-08 19:46:32 -0300

    Inserir e buscar cliente

--- app/views.py
+++ app/views.py
@@ -1,10 +1,10 @@
-from flask import render_template 
+from flask import render_template, request, redirect, url_for 
 from app import app 
 from flask_mysqldb import MySQL
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'ra10fa20'
+app.config['MYSQL_PASSWORD'] = '123'
 app.config['MYSQL_DB'] = 'sys'
 
 mysql = MySQL(app)
@@ -25,6 +25,36 @@ def buscarcliente():
 @app.route('/cadastrarcliente')
 def cadastrarcliente():
 	return render_template('cadastrarcliente.html')
+
+@app.route('/buscarcliente/bd', methods=['POST'])
+def retornarcliente():
+	cpf = request.form['cpf_busca']
+	conn = mysql.connection
+	cur = conn.cursor()
+	query = """SELECT * FROM Cliente c WHERE c.cpf = %s"""
+	cur.execute(query, [cpf])
+	rv = cur.fetchall();
+	return redirect(url_for('index'))
+
+@app.route('/cadastrarcliente/bd' , methods=['POST'])
+def inserircliente():
+	name = request.form['in_nome']	
+	cpf = request.form['in_cpf']
+	tel = request.form['in_tel']
+	rua = request.form['in_rua']
+	num = request.form['in_n']
+	cidade = request.form['in_cidade']
+	cep = request.form['in_cep']
+	estado = request.form['in_estado']
+	senha = request.form['in_senha']
+	print(name,cpf)
+	conn = mysql.connection
+	cur = conn.cursor()
+	cur.execute('''INSERT INTO Endereco VALUES (%s,%s,%s,%s,%s)''',(rua,num,cep,cidade,estado))
+	cur.execute('''INSERT INTO Cliente VALUES (%s)''',(cpf))
+	conn.commit()
+	return render_template('index.html')
+
 
 @app.route('/veiculo')
 def veiculo():
